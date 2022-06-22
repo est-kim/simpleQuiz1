728c69aeba95a13141ad8315cdca51d5ff7b4db2
Esther Kim <kim.esther117@gmail.com>
2022-06-22 13:43:28 -0700

    Submitting button with score update

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const submit = document.querySelector('#submit')
+const retry = document.querySelector('#retry')
 const q1choices = document.querySelectorAll('input[name="q1"]')
 const q2choices = document.querySelectorAll('input[name="q2"]')
 const results = document.getElementById('results')
 const answers = document.getElementsByClassName('answers')
+console.log(answers)
 const answerKey = document.getElementById('answerKey')
 const output = document.getElementById('output')
 
+const correctBonus = 10
 let quizRunning = true
 let score = 0
-const correctBonus = 10
+let isGameOver = false
+retry.style.visibility = 'hidden'
 
 // answerKey.style.visibility = 'hidden'
 
@@ -17,7 +21,7 @@ const newQuiz = () => {
   // answerKey.style.visibility = 'hidden'
   quizRunning = true
   for (let i = 0; i < answers.length; i++) {
-    answers[i].checked = false
+    answers[i].checked = false //redefine answers to select input tags, not ul's
   }
 }
 newQuiz()
@@ -30,6 +34,8 @@ newQuiz()
 submit.addEventListener('click', function() {
   let selectedAnswer1
   let selectedAnswer2
+  isGameOver = true
+  retry.style.visibility = 'visible'
   for (const q1choice of q1choices) {
     if (q1choice.checked) {
       selectedAnswer1 = q1choice.value
@@ -57,3 +63,10 @@ incrementScore = num => {
   score += num
   results.innerText = score
 }
+
+retry.addEventListener('click', function() {
+  isGameOver = false
+  retry.style.visibility = 'hidden'
+  q1choices.checked = false
+  q2choices.checked = false
+})
